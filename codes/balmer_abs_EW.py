0ef2272fdd32471b0e72797c63b0af85a7c949e6
JavieraTGrey <javieratgrey@gmail.com>
2026-08-18 10:59:23 -0400

    minor flake8 corrections

--- codes/balmer_abs_EW.py
+++ codes/balmer_abs_EW.py
@@ -12,8 +12,8 @@ from GALAXIES import get_galaxy, list_galaxies
 from GaussianFitting import fitSpectrum
 
 proj_DIR = '/Users/javieratoro/Desktop/thesis/proyecto 2024-2/'
-balmer_lines = ['H1_4340A', 'H1_4102A',
-                'H1_3970A', 'H1_3889A', 'H1_3835A', 'H1_3798A', 'H1_3771A', 'H1_3750A']
+balmer_lines = ['H1_4340A', 'H1_4102A', 'H1_3970A', 'H1_3889A',
+                'H1_3835A', 'H1_3798A', 'H1_3771A', 'H1_3750A']
 
 
 # Read data for galaxies
@@ -377,7 +377,7 @@ def balmer_absorption_correction(info):
     class_ = SPECTRALDATA(info)
     _ = REDSHIFT(class_)
     lines = ['H1_4340A', 'H1_4102A', 'H1_3970A', 'H1_3889A', 'H1_3835A',
-            'H1_3798A', 'H1_3771A', 'H1_3750A', 'H1_3734A', 'H1_3722A']
+             'H1_3798A', 'H1_3771A', 'H1_3750A', 'H1_3734A', 'H1_3722A']
     windows = [50, 50, 50, 50, 50, 50, 50, 50, 50, 50]
 
     fits, comps, stamps, emcee = [], [], [], []
@@ -438,8 +438,8 @@ def balmer_absorption_correction(info):
 # Program
 #
 # =============================================================================
-# if __name__ == '__main__':
-#     galaxies = list_galaxies()
-#     for gal in galaxies:
-#         info = get_galaxy(gal)
-#         balmer_absorption_correction(info)
+if __name__ == '__main__':
+    galaxies = list_galaxies()
+    for gal in galaxies:
+        info = get_galaxy(gal)
+        balmer_absorption_correction(info)
